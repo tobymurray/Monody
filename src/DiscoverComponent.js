3eee879635681b2011bbf484c0ae1d85b720e50d
Toby Murray <murray.toby+github@gmail.com>
2018-07-15 13:36:54 -0400

    Add some placeholder venue types

--- src/DiscoverComponent.js
+++ src/DiscoverComponent.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { Form, Header, Container } from 'semantic-ui-react'
+import { venues } from './Venues'
 
-const DiscoverComponent = () => (
-    <div>
-        What are you looking for?
+class DiscoverComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialItems: venues,
+      items: venues
+    };
+  }
 
-        There'll be some stuff here eventually, maybe...
-    </div>
-)
+  filterList = (event) => {
+    var updatedList = this.state.initialItems.filter((item) => {
+      return item.text.toLowerCase().search(event.target.value.toLowerCase()) !== -1;
+    });
+
+    this.setState({ items: updatedList });
+  }
+
+  render() {
+    return (
+      <Container text>
+        <Form>
+          <Header>Find something...</Header>
+          <Form.Field onChange={this.filterList}>
+            <input placeholder='What are you looking for?' />
+          </Form.Field>
+        </Form>
+        <ul>
+          {
+            this.state.items.map((item) => {
+              return <li data-category={item.value} key={item.key}>{item.text}</li>
+            })
+          }
+        </ul>
+      </Container>
+    );
+  }
+}
 
 export default DiscoverComponent
