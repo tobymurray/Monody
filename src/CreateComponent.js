3ea6bc61a470d9c7b5bbe02064bd6f8bfe4a77e9
Toby Murray <murray.toby+github@gmail.com>
2018-07-22 13:47:17 -0400

    Remove artist and industry as they're not hooked up yet

--- src/CreateComponent.js
+++ src/CreateComponent.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Tab, Header } from 'semantic-ui-react'
 
-import ArtistForm from './ArtistForm';
-import IndustryForm from './IndustryForm';
+// import ArtistForm from './ArtistForm';
+// import IndustryForm from './IndustryForm';
 import VenueForm from './VenueForm';
 
 const panes = [
-  { menuItem: 'Artist', render: () => <Tab.Pane><ArtistForm /></Tab.Pane> },
-  { menuItem: 'Industry', render: () => <Tab.Pane><IndustryForm /></Tab.Pane> },
+  // { menuItem: 'Artist', render: () => <Tab.Pane><ArtistForm /></Tab.Pane> },
+  // { menuItem: 'Industry', render: () => <Tab.Pane><IndustryForm /></Tab.Pane> },
   { menuItem: 'Venue', render: () => <Tab.Pane><VenueForm /></Tab.Pane> },
 ]
 
